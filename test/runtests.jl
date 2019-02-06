3bdd3556ba10d11d16d7373943505d5f972bb3e0
lassepe <lasse.peters@tuhh.de>
2019-02-05 22:16:10 -0800

    Add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,10 @@
+using Test
+
 using POMDPGifs
 using POMDPModels
 using Cairo # to make Compose work
 
-makegif(SimpleGridWorld())
+@testset "Basic call checks" begin
+  @test_nowarn makegif(SimpleGridWorld())
+  @test_nowarn makegif(SimpleGridWorld(), extra_initial=true, extra_final=true)
+end
