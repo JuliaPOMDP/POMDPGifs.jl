ef00a852bac72465889191784e6a01465f2bc5d5
Zachary Sunberg <sunbergzach@gmail.com>
2019-02-14 11:37:06 -0800

    fixed indentation

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,6 @@ using POMDPModels
 using Cairo # to make Compose work
 
 @testset "Basic call checks" begin
-  @test_nowarn makegif(SimpleGridWorld())
-  @test_nowarn makegif(SimpleGridWorld(), extra_initial=true, extra_final=true)
+    @test_nowarn makegif(SimpleGridWorld())
+    @test_nowarn makegif(SimpleGridWorld(), extra_initial=true, extra_final=true)
 end
