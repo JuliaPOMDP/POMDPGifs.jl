5026136545e5666546d2a4d4708584f349e82ad8
Dylan Asmar <asmar@stanford.edu>
2023-12-19 11:29:40 -0700

    added mktempdir for more robust cleanup and added explicit check of file existence

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,37 +5,27 @@ using POMDPModels
 using Cairo # to make Compose work
 
 @testset "Basic call checks" begin
-    try
-        makegif(SimpleGridWorld(), filename="test1.gif")
-        rm("test1.gif")
-        @test true
-    catch
-        @test false
-    end
-    
-    try
+    mktempdir() do dir
+        makegif(SimpleGridWorld(), filename=joinpath(dir, "test1.gif"))
+        @test isfile(joinpath(dir, "test1.gif"))
+        
         makegif(SimpleGridWorld(); 
-            filename="test2.gif",
+            filename=joinpath(dir, "test2.gif"),
             spec=(:s, :a, :r),
             show_progress=true,
             max_steps=15
         )
-        rm("test2.gif")
-        @test true
-    catch
-        @test false
-    end
-    
-    try
-        g = makegif(SimpleGridWorld(), filename="test3.gif", extra_initial=true, extra_final=true)
+        @test isfile(joinpath(dir, "test2.gif"))
+        
+        g = makegif(SimpleGridWorld(), 
+            filename=joinpath(dir, "test3.gif"), 
+            extra_initial=true, 
+            extra_final=true
+        )
 
         io = IOBuffer()
         Base.show(io, MIME("text/html"), g)
-        @test String(take!(io)) == "<img src=\"test3.gif\">\n"
-        
-        rm("test3.gif")
-        @test true
-    catch
-        @test false
+        @test String(take!(io)) == "<img src=\"$(joinpath(dir, "test3.gif"))\">\n"
+        @test isfile(joinpath(dir, "test3.gif"))
     end
 end
