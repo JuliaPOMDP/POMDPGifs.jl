8e821899a42bfe1a811b7c4e4e05b12fd6796e15
Dylan Asmar <asmar@stanford.edu>
2023-12-18 17:16:39 -0700

    restructure of tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,18 @@ using POMDPModels
 using Cairo # to make Compose work
 
 @testset "Basic call checks" begin
-    @test_nowarn makegif(SimpleGridWorld())
-    @test_nowarn makegif(SimpleGridWorld(), extra_initial=true, extra_final=true)
+    try
+        makegif(SimpleGridWorld(), filename="test.gif")
+        rm("test.gif")
+        @test true
+    catch
+        @test false
+    end
+    try
+        makegif(SimpleGridWorld(), filename="test.gif", extra_initial=true, extra_final=true)
+        rm("test.gif")
+        @test true
+    catch
+        @test false
+    end
 end
